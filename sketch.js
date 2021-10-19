b814d4905a10bb47ada0090ade76ed8fb2c5eb1b
UsedMosquito6 <90922372+UsedMosquito6@users.noreply.github.com>
2021-10-19 11:02:09 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,11 +5,51 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let x, y;
+let theta = 0;
+let speed = 4;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(800, 600);
+  angleMode(DEGREES);
+  x = width / 2;
+  y = height /2;
+  
 }
 
 function draw() {
   background(220);
+  moveShip();
+  displayShip();
+}
+
+function displayShip() {
+  push();
+  translate(x, y);
+  rotate(theta);
+  beginShape();
+  vertex(20, 0);
+  vertex(-10, 10);
+  vertex(0, 0);
+  vertex(-10, -10);
+  vertex(20, 0);
+  endShape();
+  pop();
+}
+
+function moveShip() {
+  if (keyIsDown(UP_ARROW)) {
+    startMoving();
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    theta += 5;
+  }
+  if (keyIsDown(LEFT_ARROW)) {
+    theta -= 5;
+  }
+}
+
+function startMoving() {
+  x += cos(theta) * speed;
+  y += sin(theta) * speed;
 }
