6f966cdf4ca2b7a1df68e1589ac34d86a9d42659
UsedMosquito6 <90922372+UsedMosquito6@users.noreply.github.com>
2021-10-22 11:01:26 -0600

    Added shooting and teleportation.

--- sketch.js
+++ sketch.js
@@ -5,16 +5,17 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-
-// let dx = 0;
-// let dy = 0;
-
-let x, y;
+let x, y, ay;
 let theta = 0;
 let vx = 0;
 let vy = 0;
-let ay;
 let friction = 0.99;
+let maxSpeed = 8;
+let bSpeed = 8;
+let shotX;
+let shotY;
+let ballArray = [];
+
 
 function setup() {
   createCanvas(800, 600);
@@ -28,10 +29,14 @@ function draw() {
   background(220);
   moveShip();
   displayShip();
+  checkIfOnScreen();
+  displayBall();
+  moveBall();
 }
 
 function displayShip() {
   push();
+  fill("white");
   translate(x, y);
   rotate(theta);
   beginShape();
@@ -45,17 +50,17 @@ function displayShip() {
 }
 
 function moveShip() {
-  if (keyIsDown(UP_ARROW)) {
-    moveForward();
-  }
-  else {
-    stopMoving();
-  }
   if (keyIsDown(RIGHT_ARROW)) {
     theta += 4;
   }
   if (keyIsDown(LEFT_ARROW)) {
     theta -= 4;
+  }
+  if (keyIsDown(UP_ARROW)) {
+    moveForward();
+  }
+  else {
+    stopMoving();
   }
 }
 
@@ -64,6 +69,18 @@ function moveForward() {
   y += vy;
   vx += cos(theta) * 0.05;
   vy += sin(theta) * 0.05;
+  // if (vx > maxSpeed) {
+  //   vx = maxSpeed;
+  // }
+  // if (vx < -5) {
+  //   vx = -maxSpeed;
+  // }
+  // if (vy > maxSpeed) {
+  //   vy = maxSpeed;
+  // }
+  // if (vy < -maxSpeed) {
+  //   vy = -maxSpeed;
+  // }
 }
 
 function stopMoving() {
@@ -71,7 +88,77 @@ function stopMoving() {
   vy *= friction;
   x += vx;
   y += vy;
-  console.log(vx);
-  console.log(vy);
+}
 
+function checkIfOnScreen() {
+  if (x > width) {
+    x = 0;
+  }
+  if (x < 0) {
+    x = width;
+  }
+  if (y > height) {
+    y = 0;
+  }
+  if (y < 0) {
+    y = height;
+  }
+  for (let ball of ballArray) {
+    if (ball.bx > width) {
+      ball.bx = 0;
+    }
+    if (ball.bx < 0) {
+      ball.bx = width;
+    }
+    if (ball.by > height) {
+      ball.by = 0;
+    }
+    if (ball.by < 0) {
+      ball.by = height;
+    }
+  }
+}
+
+function spawnBall() {    //Bullet Array
+  push();
+  translate (x, y);
+  let newBall = {
+    bx: 0,
+    by: 0,
+    totalX: 0,
+    totalY: 0,
+    radius: 5,
+    ballColor: "grey",
+    changeX : cos(theta) * bSpeed,
+    changeY : sin(theta) * bSpeed,
+  };
+  ballArray.push(newBall);    
+  pop();
+}
+
+function displayBall() {
+  for (let ball of ballArray) {
+    fill(ball.ballColor);
+    circle(ball.bx, ball.by, ball.radius);
+  }
+}
+
+function moveBall() {
+  for (let theBall of ballArray) {
+    theBall.bx += theBall.changeX;
+    theBall.by += theBall.changeY;
+    theBall.totalX += theBall.changeX;
+    theBall.totalY += theBall.changeY;
+    if (theBall.totalX > 550 || theBall.totalX < -550 || theBall.totalY > 550 || theBall.totalY < -550) {
+      ballArray.shift();
+    }
+  }
+}
+//When I added this function, I couldnt move and shoot at the same time
+function keyPressed() { //Shoot   
+  if (keyCode === 32) {
+    spawnBall();
+    ballArray[ballArray.length-1].bx = x;
+    ballArray[ballArray.length-1].by = y;
+  }
 }
