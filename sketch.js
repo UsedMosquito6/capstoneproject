38e2cbd92a20ed82b852d29594c72baf2b401b28
UsedMosquito6 <90922372+UsedMosquito6@users.noreply.github.com>
2021-10-25 11:01:25 -0600

    Added random asteroids.

--- sketch.js
+++ sketch.js
@@ -15,23 +15,30 @@ let bSpeed = 8;
 let shotX;
 let shotY;
 let ballArray = [];
-
+let asteroidArray= [];
+let thrust = false;
+let side;
 
 function setup() {
   createCanvas(800, 600);
   angleMode(DEGREES);
   x = width / 2;
   y = height /2;
-  
 }
 
 function draw() {
   background(220);
+
+  displayBall();
+  moveBall();
+
+  displayAsteroid();
+  moveAsteroid();
+
   moveShip();
   displayShip();
   checkIfOnScreen();
-  displayBall();
-  moveBall();
+
 }
 
 function displayShip() {
@@ -39,7 +46,15 @@ function displayShip() {
   fill("white");
   translate(x, y);
   rotate(theta);
-  beginShape();
+  if (thrust === true) {  // Rocket flame
+    beginShape();
+    vertex(0,0);
+    vertex(-5,-5);
+    vertex(-15,0);
+    vertex(-5,5);
+    endShape();
+  }
+  beginShape();  // Ship
   vertex(15, 0);
   vertex(-15, 10);
   vertex(-5, 0);
@@ -49,7 +64,7 @@ function displayShip() {
   pop();
 }
 
-function moveShip() {
+function moveShip() { // Movement
   if (keyIsDown(RIGHT_ARROW)) {
     theta += 4;
   }
@@ -58,9 +73,14 @@ function moveShip() {
   }
   if (keyIsDown(UP_ARROW)) {
     moveForward();
+    thrust = true;
   }
   else {
     stopMoving();
+    thrust = false;
+  }
+  if (keyIsDown(49)) {
+    spawnAsteroid();
   }
 }
 
@@ -117,9 +137,23 @@ function checkIfOnScreen() {
       ball.by = height;
     }
   }
+  for (let asteroid of asteroidArray) {
+    if (asteroid.x > width + asteroid.radius) {
+      asteroid.x = 0;
+    }
+    if (asteroid.x < 0 - asteroid.radius) {
+      asteroid.x = width;
+    }
+    if (asteroid.y > height + asteroid.radius) {
+      asteroid.y = 0;
+    }
+    if (asteroid.y < 0 - asteroid.radius) {
+      asteroid.y = height;
+    }
+  }
 }
 
-function spawnBall() {    //Bullet Array
+function spawnBall() {  //Bullet Array
   push();
   translate (x, y);
   let newBall = {
@@ -127,10 +161,10 @@ function spawnBall() {    //Bullet Array
     by: 0,
     totalX: 0,
     totalY: 0,
-    radius: 5,
+    radius: 3,
     ballColor: "grey",
-    changeX : cos(theta) * bSpeed,
-    changeY : sin(theta) * bSpeed,
+    changeX: cos(theta) * bSpeed,
+    changeY: sin(theta) * bSpeed,
   };
   ballArray.push(newBall);    
   pop();
@@ -154,11 +188,72 @@ function moveBall() {
     }
   }
 }
-//When I added this function, I couldnt move and shoot at the same time
+
 function keyPressed() { //Shoot   
   if (keyCode === 32) {
     spawnBall();
     ballArray[ballArray.length-1].bx = x;
     ballArray[ballArray.length-1].by = y;
   }
+}
+
+
+
+function spawnAsteroid() {  //Asteroid Array
+  let newAsteroid = {
+    x: 0,
+    y: 0,
+    radius: 50,
+    asteroidColor: "grey",
+    side: round(random(1,4)),
+    changeX: cos(random(360)) * 2,
+    changeY: sin(random(360)) * 2,
+  };
+  for (let asteroid of asteroidArray) {
+    if (asteroid.side === 1) {
+      asteroid.x = random(width);
+      asteroid.y = 0 - asteroid.radius;
+    }
+    if (asteroid.side === 2) {
+      asteroid.x = width + asteroid.radius;
+      asteroid.y = random(height);
+    }
+    if (asteroid.side === 3) {
+      asteroid.x = random(width);
+      asteroid.y = height + asteroid.radius;
+    }
+    if (asteroid.side === 4) {
+      asteroid.x = 0 - asteroid.radius;
+      asteroid.y = random(height);
+    }
+  }
+  asteroidArray.push(newAsteroid);    
+}
+
+function displayAsteroid() {
+  for (let i = 0; i<ballArray.length; i++){
+    for (let j = 0; j < asteroidArray.length; j++){
+      checkColisionWith(asteroidArray[j], ballArray[i]);
+    }
+  }
+  for (let asteroid of asteroidArray) {
+    fill(asteroid.asteroidColor);
+    circle(asteroid.x, asteroid.y, asteroid.radius);
+  }
+}
+
+function moveAsteroid() {
+  for (let theAsteroid of asteroidArray) {
+    theAsteroid.x += theAsteroid.changeX;
+    theAsteroid.y += theAsteroid.changeY;
+  }
+}
+
+function checkColisionWith(asteroid, ball) {
+  let distanceBetween = dist(ball.x, ball.y, asteroid.x, asteroid.y);
+  let radiSum = ball.radius + asteroid.radius;
+  if (distanceBetween < radiSum){
+    asteroidArray.delete(asteroid);
+  }
+
 }
